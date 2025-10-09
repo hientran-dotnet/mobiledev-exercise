2070c7e560173cdaee9310b95e0fb549855b94d0
HienTran <hientm0978@gmail.com>
2025-10-09 18:01:43 +0700

    Refractor code bai 3

--- Tuan_3/UI_Component/app/src/main/java/com/example/ui_component/MainActivity.kt
+++ Tuan_3/UI_Component/app/src/main/java/com/example/ui_component/MainActivity.kt
@@ -1,22 +1,22 @@
-package com.example.simpleuijpc
+package com.example.ui_component
 
 import android.annotation.SuppressLint
+import android.graphics.Paint
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -27,15 +27,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.simpleuijpc.ui.theme.SimpleUIJPCTheme
-import java.nio.file.WatchEvent
+import com.example.ui_component.ui.theme.UI_ComponentTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -50,63 +49,105 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Preview(
     name = "Pixel 6 Pro Preview",
     showBackground = true,
     showSystemUi = true,
     device = "id:pixel_6_pro"
 )
+
 @Composable
 fun MainLayout(){
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp)
-            .padding()
+            .padding(30.dp)
     ){
+
+
+        // Khung layout chính
         Column (
-            modifier = Modifier.align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier,
+//            horizontalAlignment = Alignment.CenterHorizontally
         ){
+            // Khung info của người dùng
+//            Row(
+//                modifier = Modifier,
+//                horizontalArrangement = Arrangement.End
+//            ){
+//
+//            }
+            Column (
+                modifier = Modifier
+                    .padding()
+                    .fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+            ){
+                // Tên người dùng
+                Text(
+                    text = "Trần Minh Hiền",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                )
+                // Mã người dùng
+                Text(
+                    text = "08325000978",
+                    textAlign = TextAlign.End,
+                    fontWeight = FontWeight.Light,
+                )
+            }
+
             Image(
                 painter = painterResource(R.drawable.jetpackcompose),
                 contentDescription = "Jetpack Compose Logo",
-                modifier = Modifier.size(240.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(240.dp)
             )
-            Spacer(modifier = Modifier.height(24.dp))
-
+            Spacer(modifier = Modifier.height(120.dp))
+            Text(
+                text = "",
+                fontSize = 36.sp,
+            )
+//            Spacer(modifier = Modifier.height(1.dp))
             // Text
             Text(
+                modifier = Modifier.fillMaxWidth(),
                 text = "Jetpack Compose",
+                textAlign = TextAlign.Center,
                 fontWeight = FontWeight.Bold,
                 fontSize = 20.sp
             )
-            Spacer(modifier = Modifier.height(8.dp))
+
+            Spacer(modifier = Modifier.height(12.dp))
 
             // Description
             Text(
+                modifier = Modifier.fillMaxWidth(),
                 text = "Jetpack Compose is a modern UI toolkit for building native Android applications using a declarative programming approach.",
                 textAlign = TextAlign.Center,
                 color = Color.Gray,
                 fontSize = 14.sp
             )
 
+            Button(
+                onClick = {
+                },
+                modifier = Modifier
+//                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .safeDrawingPadding()
+                    .height(56.dp),
+
+                shape = RoundedCornerShape(28.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF2196F3)
+                )
+            ) {
+                Text("I'm ready")
+            }
         }
 
-        Button(
-            onClick = { /* TODO */ },
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-                .height(56.dp),
-            shape = RoundedCornerShape(28.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF2196F3)
-            )
-        ) {
-            Text("I'm ready")
-        }
+
     }
 }
-
