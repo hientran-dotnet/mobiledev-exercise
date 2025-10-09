1def887e8601b709e2b73d45c68d75bf81e96ad6
HienTran <hientm0978@gmail.com>
2025-10-09 15:28:48 +0700

    Xong giao diện sử dụng jetpack compose đơn giản

---
+++ Tuan_3/SimpleUIJPC/app/src/main/java/com/example/simpleuijpc/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.simpleuijpc
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.simpleuijpc.ui.theme.SimpleUIJPCTheme
+import java.nio.file.WatchEvent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+
+@Preview(
+    name = "Pixel 6 Pro Preview",
+    showBackground = true,
+    showSystemUi = true,
+    device = "id:pixel_6_pro"
+)
+@Composable
+fun MainLayout(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ){
+        Column (
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                painter = painterResource(R.drawable.jetpackcompose),
+                contentDescription = "Jetpack Compose Logo",
+                modifier = Modifier.size(360.dp)
+            )
+            Spacer(modifier = Modifier.height(42.dp))
+
+            // Text
+            Text(
+                text = "Jetpack Compose",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Description
+            Text(
+                text = "Jetpack Compose is a modern UI toolkit for building native Android applications using a declarative programming approach.",
+                textAlign = TextAlign.Center,
+                color = Color.Gray,
+                fontSize = 14.sp
+            )
+
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(56.dp),
+            shape = RoundedCornerShape(28.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2196F3))
+        ) {
+            Text("I'm Ready")
+        }
+    }
+}
+
