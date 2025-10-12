9b8b4b87afea3d4c6c28305ab15010a66ef70033
HienTran <hientm0978@gmail.com>
2025-10-13 01:15:10 +0700

    Thêm trang text detail component và trang images

--- Tuan_3/Navigation_/app/src/main/java/com/example/navigation_/MainActivity.kt
+++ Tuan_3/Navigation_/app/src/main/java/com/example/navigation_/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,30 +25,43 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -56,9 +71,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import androidx.room.util.copy
+import androidx.window.core.layout.WindowWidthSizeClass
+import coil.compose.AsyncImage
 import com.example.navigation_.Screen.MainLayout.route
 import com.example.navigation_.ui.theme.FontRoboto_Bold
 import com.example.navigation_.ui.theme.FontRoboto_regular
+import com.example.navigation_.ui.theme.FontSourceCodePro
 import com.example.navigation_.ui.theme.FrontPoppins
 import com.example.navigation_.ui.theme.FrontPoppinsBold
 import com.example.navigation_.ui.theme.Navigation_Theme
@@ -86,7 +104,22 @@ class MainActivity : ComponentActivity() {
                             UiComponentScreen(navController = navController)
                         }
                         composable (route = Screen.TextDetailScreen.route){
-                            TextDetailScreen()
+                            TextDetailScreen(navController = navController)
+                        }
+                        composable(route = Screen.ImageScreen.route){
+                            ImageScreen(navController = navController)
+                        }
+                        composable(route = Screen.TextFieldScreen.route){
+                            TextFieldScreen()
+                        }
+                        composable(route = Screen.PasswordFieldScreen.route){
+                            PasswordFieldScreen()
+                        }
+                        composable(route = Screen.ColumnScreen.route){
+                            ColumnScreen()
+                        }
+                        composable(route = Screen.RowScreen.route){
+                            RowScreen()
                         }
                     }
                 }
@@ -183,20 +216,15 @@ fun MainLayout(navController: NavController) {
     }
 }
 
-
-@Composable
-fun MainLayoutPreview() {
-    val navController = rememberNavController()
-    MainLayout(navController = navController)
-}
-
-
 @Composable
 fun UiComponentScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
+//            .padding(24.dp),
+            .statusBarsPadding()
+            .padding(10.dp),
+
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -298,6 +326,9 @@ fun UiComponentScreen(navController: NavController) {
                                 Color(0x4D2196F3),
                                 shape = RoundedCornerShape(10.dp)
                             )
+                            .clickable{
+                                navController.navigate(Screen.ImageScreen.route)
+                            }
                     ){
                         Column(
                             modifier = Modifier
@@ -324,13 +355,11 @@ fun UiComponentScreen(navController: NavController) {
                                     .fillMaxWidth(),
                                 textAlign = TextAlign.Start,
                                 style = FontRoboto_regular,
-//                                fontWeight = FontWeight.Bold,
                                 fontSize = 18.sp
                             )
                         }
                     }
 
-//                    Spacer(modifier = Modifier.height(27.dp))
 
                     Text(
                         text = "Input",
@@ -342,7 +371,6 @@ fun UiComponentScreen(navController: NavController) {
                         fontSize = 16.sp
                     )
 
-//                    Spacer(modifier = Modifier.height(4.dp))
 
                     Box(
                         modifier = Modifier
@@ -352,6 +380,9 @@ fun UiComponentScreen(navController: NavController) {
                                 Color(0x4D2196F3),
                                 shape = RoundedCornerShape(10.dp)
                             )
+                            .clickable{
+                                navController.navigate(Screen.TextFieldScreen.route)
+                            }
                     ){
                         Column(
                             modifier = Modifier
@@ -394,6 +425,9 @@ fun UiComponentScreen(navController: NavController) {
                                 Color(0x4D2196F3),
                                 shape = RoundedCornerShape(10.dp)
                             )
+                            .clickable{
+                                navController.navigate(Screen.PasswordFieldScreen.route)
+                            }
                     ){
                         Column(
                             modifier = Modifier
@@ -449,6 +483,9 @@ fun UiComponentScreen(navController: NavController) {
                                 Color(0x4D2196F3),
                                 shape = RoundedCornerShape(10.dp)
                             )
+                            .clickable{
+                                navController.navigate(Screen.ColumnScreen.route)
+                            }
                     ){
                         Column(
                             modifier = Modifier
@@ -491,6 +528,9 @@ fun UiComponentScreen(navController: NavController) {
                                 Color(0x4D2196F3),
                                 shape = RoundedCornerShape(10.dp)
                             )
+                            .clickable{
+                                navController.navigate(Screen.RowScreen.route)
+                            }
                     ){
                         Column(
                             modifier = Modifier
@@ -533,6 +573,9 @@ fun UiComponentScreen(navController: NavController) {
                                 Color(0x4DE80400),
                                 shape = RoundedCornerShape(10.dp)
                             )
+                            .clickable{
+
+                            }
                     ){
                         Column(
                             modifier = Modifier
@@ -570,18 +613,322 @@ fun UiComponentScreen(navController: NavController) {
     }
 }
 
-@Preview(
-    showBackground = true,
-    showSystemUi = true,
-    device = "id:pixel_6_pro",
-    name = "Pixel 6 Pro"
-)
 @Composable
-fun TextDetailScreen(){
+fun TextDetailScreen(navController: NavController){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+//            .padding(24.dp)
+        ,
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            // Nút quay lại
+            Icon(
+                painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                contentDescription = null,
+                tint = Color(0xFF2196F3),
+                modifier = Modifier
+                    .padding(start = 24.dp)
+                    .clickable(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    )
+            )
+
+            Text(
+                text = "Text Component",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 70.dp)
+                ,
+                textAlign = TextAlign.Start,
+                style = FrontPoppinsBold,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp,
+                color = Color(0xFF2196F3)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+//                .fillMaxWidth()
+                .fillMaxSize()
+            ,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = CenterHorizontally
+        ){
+
+
+            Text(
+                buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.Normal,
+                        )
+                    ){
+                        append("The ")
+                    }
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+                            textDecoration = TextDecoration.LineThrough
+                        )
+                    ){
+                        append("quick")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color(0xFFFF5722),
+                            fontSize = 80.sp,
+
+                        )
+                    ){
+                        append(" B")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color(0xFFFF5722),
+                            fontSize = 40.sp,
+
+                        )
+                    ){
+                        append("rown\n\n")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp
+                        )
+                    ){
+                        append("fox ")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+                            letterSpacing = 12.sp
+                        )
+                    ){
+                        append("jumps ")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+                            fontStyle = FontStyle.Italic,
+                            fontWeight = FontWeight.Bold
+
+                        )
+                    ){
+                        append("over\n\n")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+                            textDecoration = TextDecoration.Underline
+
+                        )
+                    ){
+                        append("the")
+                    }
+
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+                            fontFamily = FontFamily(Font(R.font.dacingscript))
+                        )
+                    ){
+                        append(" lazy")
+                    }
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontSize = 40.sp,
+//                            fontFamily = FontFamily(Font(R.font.dacingscript))
+                        )
+                    ){
+                        append(" dog")
+                    }
+
+                }
+            )
+        }
+
+    }
+
+
+}
+
+@Composable
+fun ImageScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding(),
+        contentAlignment = Alignment.Center,
+    ) {
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+
+                // Nút quay lại
+                Icon(
+                    painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                    contentDescription = null,
+                    tint = Color(0xFF2196F3),
+                    modifier = Modifier
+                        .padding(start = 24.dp)
+                        .clickable(
+                            onClick = {
+                                navController.popBackStack()
+                            }
+                        )
+                )
+
+                Text(
+                    text = "Images Component",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 70.dp)
+                    ,
+                    textAlign = TextAlign.Start,
+                    style = FrontPoppinsBold,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                    color = Color(0xFF2196F3)
+                )
+            }
+
+            AsyncImage(
+                model = "https://clc.ut.edu.vn/wp-content/uploads/2025/03/TN_00033-2048x1365.jpg",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+                    .clip(shape = RoundedCornerShape(16.dp))
+                ,
+                contentDescription = "Hình ảnh đại học giao thông vận tải"
+            )
+
+            Text(
+                text = buildAnnotatedString {
+                    withLink(
+                        LinkAnnotation.Url(
+                            "https://clc.ut.edu.vn/wp-content/uploads/2025/03/TN_00033-2048x1365.jpg",
+                        )
+                    ) {
+                        append("https://clc.ut.edu.vn/wp-content/uploads/2025/03/TN_00033-2048x1365.jpg")
+                    }
+                },
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+            )
+
+            Image(
+                painter = painterResource(R.drawable.uthmoi),
+                contentDescription = "Hình ảnh đại học giao thông vận tải",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+                    .clip(shape = RoundedCornerShape(16.dp))
+            )
+
+            Text(
+                text = "In app",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+            )
+        }
+    }
+}
+
+
+@Composable
+fun TextFieldScreen(){
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text("Đây là trang chi tiết!")
+        Text("Đây là trang input text component")
     }
 }
+
+@Composable
+fun PasswordFieldScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Đây là trang input password component!")
+    }
+}
+
+@Composable
+fun ColumnScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Đây là trang column layout!")
+    }
+}
+
+@Composable
+fun RowScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Đây là trang row layout!")
+    }
+}
+
+//@Composable
+//fun responsivePadding(
+//    compact: Dp = 16.dp,
+//    medium: Dp = 24.dp,
+//    expanded: Dp = 32.dp
+//): Dp {
+//    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+//
+//    return when (windowSizeClass.windowWidthSizeClass) {
+//        WindowWidthSizeClass.COMPACT -> compact
+//        WindowWidthSizeClass.MEDIUM -> medium
+//        WindowWidthSizeClass.EXPANDED -> expanded
+//        else -> medium
+//    }
+//}
--- Tuan_3/Navigation_/app/src/main/java/com/example/navigation_/Screen.kt
+++ Tuan_3/Navigation_/app/src/main/java/com/example/navigation_/Screen.kt
@@ -5,4 +5,15 @@ sealed class Screen(val route: String){
     object  UiComponent : Screen("ui_component")
 
     object TextDetailScreen : Screen("text_detail_screen")
+
+    object ImageScreen : Screen("image_detail_screen")
+
+    object TextFieldScreen : Screen("text_field_detail_screen")
+
+    object PasswordFieldScreen : Screen("password_field_detail_screen")
+
+    object ColumnScreen : Screen("column_detail_screen")
+
+    object RowScreen : Screen("row_detail_screen")
+
 }
