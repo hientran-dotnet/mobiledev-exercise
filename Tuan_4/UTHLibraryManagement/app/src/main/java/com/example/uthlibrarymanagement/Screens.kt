46f0b0ee7d8d8b844c506167c3292b63c70cad66
HienTran <hientm0978@gmail.com>
2025-10-17 12:32:21 +0700

    remove dự án cũ

--- Tuan_4/UTHLibraryManagement/app/src/main/java/com/example/uthlibrarymanagement/Screens.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uthlibrarymanagement
-
-sealed class Screen(val route: String){
-    object MainLayout : Screen("main_layout")
-    object BooksList : Screen(route = "books_list")
-    object StudentsList : Screen(route = "students_list")
-
-}
--- Tuan_4/UTHLibraryManagement/app/src/main/java/com/example/uthlibrarymanagement/navItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uthlibrarymanagement
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class navItem(
-    val title: String,
-    val icon: ImageVector,
-)
